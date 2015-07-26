b37f585049f3f7d677cad5ef5336e3e82d6b59b8
Nguoi An Phu <nguoianphu@gmail.com>
2015-07-26 20:19:31 +0700

    edit pom.xml

--- src/main/java/com/TestTitle.java
+++ src/main/java/com/TestTitle.java
@@ -1,5 +1,7 @@
-package test;
+package com;
 
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
@@ -8,17 +10,21 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 
 public class TestTitle {
-	private WebDriver driver;
+	private WebDriver driver = null;
+	public String URL = "http://www.nguoianphu.com/user/login";
 
 	@Test
 	public void testEasy() {
-		driver.get("http://www.guru99.com/selenium-tutorial.html");
+		driver.get(URL);
 		String title = driver.getTitle();
-		Assert.assertTrue(title.contains("Free Selenium Tutorials"));
+		// AssertJUnit.assertTrue(title.contains("Home page Nguoi An Phu huyen An Phu tinh An Giang"));
+		AssertJUnit
+				.assertTrue(title
+						.contains("User login Nguoi An Phu huyen An Phu tinh An Giang"));
 	}
 
 	@BeforeTest
-	public void beforeTest() {
+	public void beforeTest() throws Exception {
 		driver = new FirefoxDriver();
 	}
 
