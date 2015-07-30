128e2dbd38be7ffe7825aee52b2e65f389b7eccd
Nguoi An Phu <nguoianphu@gmail.com>
2015-07-30 18:24:02 +0700

    Passing Parameters with testng.xml

--- src/main/java/com/TestTitle.java
+++ src/main/java/com/TestTitle.java
@@ -4,22 +4,23 @@ import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
-
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.Parameters;
 
 public class TestTitle {
 	private WebDriver driver = null;
-	// public String URL = "http://www.nguoianphu.com/user/login";
-	public String URL = "http://www.nguoianphu.com";
+
+	// public String URL = "http://www.nguoianphu.com";
 
 	@Test
-	public void testEasy() {
-		driver.get(URL);
+	@Parameters({ "myURL", "myTitle" })
+	public void testEasy(String myURL, String myTitle) {
+		driver.get(myURL);
 		String title = driver.getTitle();
-		Assert.assertTrue(title.contains("Home page Nguoi An Phu huyen An Phu tinh An Giang"));
-		//Assert.assertTrue(title
-		//		.contains("User login Nguoi An Phu huyen An Phu tinh An Giang"));
+		Assert.assertTrue(title.contains(myTitle));
+		// Assert.assertTrue(title
+		// .contains("Home page Nguoi An Phu huyen An Phu tinh An Giang"));
 	}
 
 	@BeforeTest
